d1d09c69eafb05b0546582b6348e865909a62e78
denghang <denghang@huiwtech.com>
2020-02-15 12:53:17 +0800

    项目初始化

---
+++ src/main/java/com/huiwtech/film/FilmApplication.java
@@ -0,0 +1,13 @@
+package com.huiwtech.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huiwtech/film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huiwtech.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
